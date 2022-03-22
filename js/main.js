55a9a84bc7ea26b053894dfc7b0f6a49d92c5cd9
Konrad <konrad.surowiec3@wp.pl>
2022-03-22 09:15:16 +0100

    created box modal and added change of degrees, moreover added funcionality for app

--- js/main.js
+++ js/main.js
@@ -2,54 +2,71 @@ const inputNumber = document.querySelector('#input-set');
 const btnCheck = document.querySelector('#btn-check');
 const btnChange = document.querySelector('#btn-change');
 const score = document.querySelector('#score');
-const titleApp = document.querySelector('#title');
+const subtitleApp = document.querySelector('#subtitle');
 
+const btnChangeCel =document.querySelector('.converter__btn-deg-cel');
+const btnChangeFar =document.querySelector('.converter__btn-deg-far');
 
+const bcgCover = document.querySelector('.background-cover');
 
-let celsius 
+const modal = document.querySelector('.modal');
+const modalMsg = document.querySelector('.modal__text');
+const modalBtn = document.querySelector('.modal__cross');
+
+let degrees 
 let temp
 
-
+//shows modal and cover background
+const handleModal = (msg) => {
+    modal.classList.add('modal--active');
+    modalMsg.textContent = `${msg}`;
+    bcgCover.classList.add('background-cover--active');
+    modalBtn.addEventListener('click', ()=>{
+            modal.classList.remove('modal--active');
+            bcgCover.classList.remove('background-cover--active');
+    })
+}
+//computes the swap
 const handleConversion = () => {
-    const fahrenheit = () => {
-        celsius = inputNumber.value;
-        if(celsius != ""){
-            temp = celsius * 1.8 + 32;
+    const scoreFahrenheit = () => {
+        degrees = inputNumber.value;
+        if(degrees != ""){
+            temp = degrees * 1.8 + 32;
             score.textContent = temp;
         } else {
-            alert("Musisz podać jakąś wartość!")
+            handleModal("Musisz wpisać jakąś wartość!");
         }
     }
-    fahrenheit();
+    
+    const scoreCelsjus = () => {
+        degrees = inputNumber.value;
+        if(degrees != ""){
+            temp = 5 / 9 * (degrees - 32)
+            score.textContent = temp;
+        } else {
+            handleModal("Musisz wpisać jakąś wartość!");
+        }
+    }
+
+    if(subtitleApp.textContent === "Celsjusz na Fahrenheit"){
+        scoreFahrenheit();
+    } else {
+        scoreCelsjus();
+    }
+}
+
+//changes the subtitle
+const subtitleCel = () =>{
+    subtitleApp.textContent = "Celsjusz na Fahrenheit";
+
+}
+
+const subtitleFar = () =>{
+    subtitleApp.textContent = "Fahrenheit na Celsjusz";
 }
 
 
 
-btnCheck.addEventListener('click', handleConversion)
-
-
-// btnArr.map(button => {
-//     if(button.classList.contains("converter__btn-check")){
-//         button.addEventListener('click', () => {
-//             console.log("Hi");
-//         })
-//     }
-//     if(button.classList.contains("converter__input")){
-//         const value = button.getAttribute("value");
-//         button.addEventListener('click', () => {
-//             console.log(value);
-//         })
-//     }
-// })
-
-
-
-
-
-
-
-
-
-
-
-
+btnChangeCel.addEventListener('click', subtitleCel);
+btnChangeFar.addEventListener('click', subtitleFar);
+btnCheck.addEventListener('click', handleConversion);
